a93de8170c337168a42bb6f2242b5bc4d9fad00a
rootrc <rootrc123@gmail.com>
2026-01-07 01:41:27 -0500

    button fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ text, href, newTab = true}) {
       href={href}
       target={newTab ? "_blank" : "_self"}
       rel={newTab ? "noopener noreferrer" : undefined}
-      classname="inline-block mt-5 px-6 py-3 text-sm sm:text-base font-semibold uppercase bg-indigo-600 text-white border border-indigo-600 rounded-sm text-center no-underline transition-all duration-200 ease-in-out hover:bg-indigo-00 hover:-translate-y-0.5 hover:shadow-md"
+      className="inline-block mt-5 px-6 py-3 text-sm sm:text-base font-semibold uppercase bg-indigo-600 text-white border border-indigo-600 rounded-sm text-center no-underline transition-all duration-200 ease-in-out hover:bg-indigo-500 hover:-translate-y-0.5 hover:shadow-md"
     >
       {text}
     </a>
